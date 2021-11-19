eadd892c9eb0fccf1232bf14a3e337d5ab21d34a
Michael Evko <mbevko@gmail.com>
2021-11-18 23:43:23 -0500

    started AR module

--- src/modules/AccountsReceivable.js
+++ src/modules/AccountsReceivable.js
@@ -16,7 +16,67 @@ const AccountsReceivable = [
         MODULE: 'ACCOUNTS RECEIVABLE',
         TABLE: 'ARBTA',
         TITLE: 'RECEIPT AND ADJUSTMENT BATCHES'
-    }
+    },
+    {
+        id: 204,
+        MODULE: 'ACCOUNTS RECEIVABLE',
+        TABLE: 'ARCCRECD',
+        TITLE: 'CREATE CREDIT CARD RECEPITS DTL'
+    },
+    {
+        id: 205,
+        MODULE: 'ACCOUNTS RECEIVABLE',
+        TABLE: 'ARCMM',
+        TITLE: 'CUSTOMER COMMENTS'
+    },
+    {
+        id: 206,
+        MODULE: 'ACCOUNTS RECEIVABLE',
+        TABLE: 'ARCMMD',
+        TITLE: 'CUSTOMER COMMENT DETAILS'
+    },
+    {
+        id: 207,
+        MODULE: 'ACCOUNTS RECEIVABLE',
+        TABLE: 'ARCMMTP',
+        TITLE: 'COMMENT TYPES'
+    },
+    {
+        id: 208,
+        MODULE: 'ACCOUNTS RECEIVABLE',
+        TABLE: 'ARCSM',
+        TITLE: 'CUSTOMER STATISTICS'
+    },
+    {
+        id: 209,
+        MODULE: 'ACCOUNTS RECEIVABLE',
+        TABLE: 'ARCSP',
+        TITLE: 'SHIP-TO LOCATIONS'
+    },
+    {
+        id: 210,
+        MODULE: 'ACCOUNTS RECEIVABLE',
+        TABLE: 'ARCSPO',
+        TITLE: 'SHIP-TO LOCATIONS OPTIONAL FIELD VALUES'
+    },
+    {
+        id: 211,
+        MODULE: 'ACCOUNTS RECEIVABLE',
+        TABLE: 'ARCUS',
+        TITLE: 'CUSTOMERS'
+    },
+    {
+        id: 212,
+        MODULE: 'ACCOUNTS RECEIVABLE',
+        TABLE: 'ARCUSC',
+        TITLE: 'CUSTOMER CONTACTS'
+    },
+    {
+        id: 213,
+        MODULE: 'ACCOUNTS RECEIVABLE',
+        TABLE: 'ARCUSCF',
+        TITLE: 'CUSTOMER CONTACT FORMS'
+    },
 ]
 
 export default AccountsReceivable
